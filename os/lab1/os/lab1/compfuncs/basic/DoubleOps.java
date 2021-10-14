8ad5a69d6d144ccddb691e6419ead308bc31dda3
fivard <bernadadmytryi@gmail.com>
2021-10-14 15:58:41 +0300

    lab_1

---
+++ os/lab1/os/lab1/compfuncs/basic/DoubleOps.java
@@ -0,0 +1,13 @@
+package compfuncs.basic;
+
+import java.util.Optional;
+
+public class DoubleOps {
+    public static Optional<Double> trialF(Integer x){
+        return Optional.of((double)x/3);
+    }
+
+    public static Optional<Double> trialG(Integer x){
+        return Optional.of((double)3/x);
+    }
+}
---
+++ os/lab1/os/lab1/compfuncs/basic/IntOps.java
@@ -0,0 +1,14 @@
+package compfuncs.basic;
+
+import java.util.Optional;
+
+public class IntOps {
+    public static Optional<Integer> trialF(Integer x){
+        return Optional.of(x * 3);
+    }
+
+    public static Optional<Integer> trialG(Integer x){
+        return Optional.of(x + 5);
+    }
+}
+
