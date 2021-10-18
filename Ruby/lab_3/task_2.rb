a3f66b344e056d22dbe098ec045e2186ab88ff17
fivard <bernadadmytryi@gmail.com>
2021-10-18 23:11:32 +0300

    finish lab_3

--- Ruby/lab_3/task_2.rb
+++ Ruby/lab_3/task_2.rb
@@ -1,18 +1,24 @@
 x = 1
 
 if x > -4 && x <= 0
-  puts ((x-2).abs / (x**2 * Math.cos(x)))**(1/7)
+  puts ((x-2).abs / (x**2 * Math.cos(x)))**(1.0/7)
 elsif x > 0 && x <= 12
-  puts 1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2)**(7/2)
+  puts 1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2)**(7.0/2)
 elsif x < -4 || x > 12
   puts 1 / (1 + x / (1 + x / (1 + x / (1 + x))))
 end
 
 case x
 when -4..0 then
-  puts ((x-2).abs / (x**2 * Math.cos(x)))**(1/7)
+  if x != -4
+    puts ((x-2).abs / (x**2 * Math.cos(x)))**(1.0/7)
+  end
 when 0..12 then
-  puts 1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2)**(7/2)
+  if x != 0
+    puts 1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2)**(7.0/2)
+  end
 else
-  puts 1 / (1 + x / (1 + x / (1 + x / (1 + x))))
+  if x != -4
+    puts 1 / (1 + x / (1 + x / (1 + x / (1 + x))))
+  end
 end
--- Ruby/lab_3/task_5.rb
+++ Ruby/lab_3/task_5.rb
@@ -1,10 +1,11 @@
-$N = 100
-$c = 12
+$N = 10
+$c = 30
+$pi = 3.141
 
 def calculate_y(x)
-  (x**(1/4) - (1/x)**(1/4))**(-$N) +
+  (x**(1.0/4) - (1/x)**(1.0/4))**(-$N) +
     (x + 1) / (x**2 - 4*x + 3*$N) +
-    ((36*x*$c*$N)**(1/4) / ((x + $c*$N + 1) / (x + 5)))
+    ((36*x*$c*$N)**(1.0/4) / ((x + $c*$N + 1) / (x + 5)))
 end
 
 def calculate_z(x)
@@ -15,3 +16,43 @@ def calculate_z(x)
     (2*x + Math.sin(3*x)**(2))
 end
 
+def output_dict(dict)
+  dict.each { |i|
+    puts i, dict[i]
+  }
+end
+
+first_answer = {}
+(1..$N).step(($N - 1).to_f / ($N + $c)) do |i|
+  first_answer[i] = calculate_y(i)
+end
+
+
+second_answer = {}
+i = $pi / $N
+delta = ($pi - $pi / $N) / ((3.0/2)*$N + $c)
+while i <= $pi do
+  second_answer[i] = calculate_z(i)
+  i += delta
+end
+
+
+third_answer = {}
+(2..$c).step(($c - 2).to_f / (2*$N)) do |j|
+  if j > 2 && j < $N
+    third_answer[j] = calculate_y(j)
+  elsif j > $N && j < 2*$N
+    third_answer[j] = calculate_z(j)
+  else
+    third_answer[j] = calculate_y(j) + calculate_z(j)
+  end
+end
+
+
+
+puts "____________FIRST__________"
+output_dict(first_answer)
+puts "____________SECOND__________"
+output_dict(second_answer)
+puts "____________THIRD__________"
+output_dict(third_answer)
