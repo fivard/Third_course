9b126f9477d4b22abf7e0c14b89a2fa4f2e57ae9
Dmytryi <57447810+fivard@users.noreply.github.com>
2021-10-25 00:13:11 +0300

    lab_2

---
+++ NumericalMethods/lab_2/gauss_method.py
@@ -0,0 +1,51 @@
+import numpy as np
+np.set_printoptions(precision=3)
+given_matrix = np.matrix('11,     7,      3,      7,      2;'
+                         '7,      10,    -1,     4,       2;'
+                         '3,      -1,     16,     -7,     -3;'
+                         '7,      4,      -7,     15,     5')
+
+
+def generate_m(matrix, iteration):
+    m = np.eye(4)
+    for i in range(iteration, 4):
+        if i == iteration:
+            m[i, iteration] = 1 / matrix[iteration, iteration]
+        else:
+            m[i, iteration] = -matrix[i, iteration] / matrix[iteration, iteration]
+
+    return m
+
+
+def generate_p(i, j):
+    e = np.eye(4)
+    e[:, [i, j]] = e[:, [j, i]]
+    return e
+
+
+def find_index_max_in_a_row(matrix, iteration):
+    return np.matrix.argmax(matrix, axis=0).item(iteration)
+
+
+def gauss_method(matrix):
+    for i in range(4):
+        print(f"\n-----------------ITERATION {i}----------------")
+        row_maximum_index = find_index_max_in_a_row(matrix, i)
+        print(f"\nIndex of the biggest number is {row_maximum_index} in {i}'s row\n")
+        p = generate_p(i, row_maximum_index)
+        print(f"\nP is :\n{p}")
+        matrix = np.matmul(p, matrix)
+        print(f"\nP * A =\n{matrix}\n")
+        m = generate_m(matrix, i)
+        print(f"\nM is :\n{m}")
+        matrix = np.matmul(m, matrix)
+        print(f"\nM * A is :\n{matrix}")
+
+    x4 = matrix[3, 4]
+    x3 = matrix[2, 4] - matrix[2, 3] * x4
+    x2 = matrix[1, 4] - matrix[1, 3] * x4 - matrix[1, 2] * x3
+    x1 = matrix[0, 4] - matrix[0, 3] * x4 - matrix[0, 2] * x3 - matrix[0, 1] * x2
+    print('\nSolution: x1 = %0.3f, x2 = %0.3f, x3 = %0.3f and x4 = %0.3f\n' % (x1, x2, x3, x4))
+
+
+gauss_method(given_matrix)
---
+++ NumericalMethods/lab_2/seidel_method.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+matrix = np.matrix('11,     7,      3,      7;'
+                   '7,      10,    -1,     4;'
+                   '3,      -1,     16,     -7;'
+                   '7,      4,      -7,     15')
+
+matrix_3 = np.matrix(f'{matrix.item(0, 0)},    {matrix.item(0, 1)},      {matrix.item(0, 2)};'
+                     f'{matrix.item(1, 0)},    {matrix.item(1, 1)},      {matrix.item(1, 2)};'
+                     f'{matrix.item(2, 0)},    {matrix.item(2, 1)},      {matrix.item(2, 2)}')
+
+matrix_2 = np.matrix(f'{matrix.item(0, 0)},    {matrix.item(0, 1)};'
+                     f'{matrix.item(1, 0)},    {matrix.item(1, 1)}')
+
+vector = [2, 2, -3, 5]
+
+
+def validation():
+    return np.linalg.det(matrix) > 0 and np.linalg.det(matrix_2) > 0 \
+           and np.linalg.det(matrix_3) > 0 and np.allclose(matrix, matrix.T)
+
+
+print('Matrix is :\n', matrix)
+if validation():
+    print("Seidel's method converges")
+else:
+    print("Seidel's method doesn't converge")
+
+
+f1 = lambda x1, x2, x3, x4: (vector[0] - (matrix.item(0, 1) * x2 + matrix.item(0, 2) * x3 + matrix.item(0, 3) * x4)) / matrix.item(0, 0)
+f2 = lambda x1, x2, x3, x4: (vector[1] - (matrix.item(1, 0) * x1 + matrix.item(1, 2) * x3 + matrix.item(1, 3) * x4)) / matrix.item(1, 1)
+f3 = lambda x1, x2, x3, x4: (vector[2] - (matrix.item(2, 0) * x1 + matrix.item(2, 1) * x2 + matrix.item(2, 3) * x4)) / matrix.item(2, 2)
+f4 = lambda x1, x2, x3, x4: (vector[3] - (matrix.item(3, 0) * x1 + matrix.item(3, 1) * x2 + matrix.item(3, 2) * x3)) / matrix.item(3, 3)
+
+x01, x02, x03, x04 = 0, 0, 0, 0
+x11, x12, x13, x14 = 0, 0, 0, 0
+count = 1
+
+e = 0.001
+
+print('\nCount\tx1\tx2\tx3\tx4\n')
+
+condition = validation()
+
+while condition:
+    x11 = f1(x01, x02, x03, x04)
+    x12 = f2(x11, x02, x03, x04)
+    x13 = f3(x11, x12, x03, x04)
+    x14 = f4(x11, x12, x13, x04)
+    print('%d\t%0.4f\t%0.4f\t%0.4f\t%0.4f\n' % (count, x11, x12, x13, x14))
+    e1 = abs(x01 - x11)
+    e2 = abs(x02 - x12)
+    e3 = abs(x03 - x13)
+    e4 = abs(x04 - x14)
+
+    count += 1
+    x01 = x11
+    x02 = x12
+    x03 = x13
+    x04 = x14
+
+    condition = max(e1, e2, e3, e4) > e
+
+if validation():
+    print('\nSolution: x1 = %0.3f, x2 = %0.3f, x3 = %0.3f and x4 = %0.3f\n' % (x11, x12, x13, x14))
+
+matrix_inv = np.linalg.inv(matrix)
+print("Number of conditionality is ", np.linalg.norm(matrix, np.inf) * np.linalg.norm(matrix_inv, np.inf))
