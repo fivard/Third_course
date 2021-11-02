1e4205e83bb8bb051dc4b370aa12240f8723a3f9
fivard <bernadadmytryi@gmail.com>
2021-11-02 09:57:16 +0200

    Ruby module

--- Ruby/first_module/task_2.rb
+++ Ruby/first_module/task_2.rb
@@ -1,7 +1,7 @@
 class House
   attr_accessor :id, :number, :square, :floor, :count_rooms, :street, :building_type, :lifetime
 
-  def initialize(id, number, square, floor, count_rooms, street, building_type, lifetime)
+  def initialize(id, number, square, floor, count_rooms, street, building_type="Lok", lifetime=100)
     @id = id
     @number = number
     @square = square
@@ -51,7 +51,7 @@ house_1 = House.new(1, 27,123,5,5, "New1", "Lok2", 10)
 house_2 = House.new(2, 20,97,1,2, "New2", "Lok5", 20)
 house_3 = House.new(3, 12,86,3,3, "New3", "Lok5", 30)
 house_4 = House.new(4, 48,23,2,2, "New4", "Lok2", 40)
-house_5 = House.new(5, 90,101,4,6, "New6", "Lok1", 50)
+house_5 = House.new(5, 90,101,4,6, "New6", "Lok1")
 
 array = [house_1, house_2, house_3, house_4, house_5]
 puts "Filter by rooms"
