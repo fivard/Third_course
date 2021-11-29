136b931861d89a6c03f5027a3e145d6acdf67ee4
fivard <bernadadmytryi@gmail.com>
2021-11-29 11:53:10 +0200

    lab_3

--- NumericalMethods/lab_5/main.py
+++ NumericalMethods/lab_5/main.py
@@ -64,22 +64,22 @@ dt = 0.2
 eps = 0.001
 timestamps = np.linspace(0, 50, 251)
 
-bb = [b0]
-while True:
-    b_curr = bb[-1].copy()
+
+def integral():
     yy = np.zeros_like(data)
     yy[0] = data[0].copy()
+    A = getA(b_curr)
     for i in range(1, len(timestamps)):
         y_prev = yy[i - 1]
-        k1 = dt * calc_f(y_prev, b_curr)
-        k2 = dt * calc_f(y_prev + 0.5 * k1, b_curr)
-        k3 = dt * calc_f(y_prev + 0.5 * k2, b_curr)
-        k4 = dt * calc_f(y_prev + k3, b_curr)
+        k1 = dt * (A @ y_prev)
+        k2 = dt * (A @ y_prev + k1 / 2)
+        k3 = dt * (A @ y_prev + k2 / 2)
+        k4 = dt * (A @ y_prev + k3)
         y = y_prev + (k1 + 2 * k2 + 2 * k3 + k4) / 6
         yy[i] = y
+
     uu = np.zeros((len(timestamps), 6, 3))
     db = calc_db(yy.T, b_curr)
-    A = getA(b_curr)
     for i in range(1, len(timestamps)):
         k1 = dt * (A @ uu[i - 1] + db[i - 1])
         k2 = dt * (A @ (uu[i - 1] + k1 / 2) + db[i - 1])
@@ -88,14 +88,22 @@ while True:
         u_next = uu[i - 1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
         uu[i] = u_next
 
-    du = (np.array([u.T @ u for u in uu]) * dt).sum(0)
-    du = np.linalg.inv(du)
-    diff_y = (data - yy)
-    uY = (np.array([uu[i].T @ diff_y[i] for i in range(len(timestamps))]) * dt).sum(0)
-    diff_beta = du @ uY
-    b_next = b_curr + diff_beta
-    bb.append(b_next)
+    return [yy, uu]
 
-    if np.abs(diff_beta).max() < eps:
+
+bb = [b0]
+
+while True:
+    b_curr = bb[-1].copy()
+    yy, uu = integral()
+
+    du = np.linalg.inv((np.array([u.T @ u for u in uu]) * dt).sum(0))
+    uY = (np.array([uu[i].T @ (data[i] - yy[i]) for i in range(len(timestamps))]) * dt).sum(0)
+    db = du @ uY
+
+    bb.append(b_curr + db)
+
+    if np.abs(db).max() < eps:
         break
+
 print(np.round(b_curr, 2))
